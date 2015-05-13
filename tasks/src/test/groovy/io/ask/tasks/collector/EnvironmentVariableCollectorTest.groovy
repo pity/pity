132754c6ed3d8f99e80edfa7376c3ec6edcb6091
Ethan Hall <ethan@ehdev.io>
2015-05-12 22:16:18 -0700

    EnvVariable collector

---
+++ tasks/src/test/groovy/io/ask/tasks/collector/EnvironmentVariableCollectorTest.groovy
@@ -0,0 +1,20 @@
+package io.ask.tasks.collector
+
+import spock.lang.Specification
+
+class EnvironmentVariableCollectorTest extends Specification {
+
+    def 'reports all env variables that do not start with _'(){
+        when:
+        def collector = new EnvironmentVariableCollector(null, ['env1': 'foo', '_env2': 'bar', 'env3': 'buzz'])
+        def environmentData = collector.collectEnvironmentData()
+
+        then:
+        environmentData.getCollectorName() == EnvironmentVariableCollector.class.getSimpleName()
+        environmentData.getEnvironmentResults().keySet() == [ 'env1', 'env3' ] as Set
+        environmentData.getEnvironmentResults()['env1'] == 'foo'
+        environmentData.getEnvironmentResults()['_env2'] == null
+        environmentData.getEnvironmentResults()['env3'] == 'buzz'
+    }
+
+}
