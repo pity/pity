4c7a4c07fce3dfdd78dbe438314184f72dae6621
Ethan Hall <ethan@ehdev.io>
2015-05-12 22:20:06 -0700

    Adding simple tests for working dir

---
+++ tasks/src/test/groovy/io/ask/tasks/collector/WorkingDirectoryEnvironmentCollectorTest.groovy
@@ -0,0 +1,31 @@
+package io.ask.tasks.collector
+
+import io.ask.api.EnvironmentData
+import io.ask.api.WorkingDirectoryProvider
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+class WorkingDirectoryEnvironmentCollectorTest extends Specification {
+
+    @Rule TemporaryFolder temporaryFolder = new TemporaryFolder()
+    WorkingDirectoryProvider workingDirectoryProvider;
+
+
+    def setup() {
+        temporaryFolder.create()
+        workingDirectoryProvider = [ 'getWorkingDirectory': { -> temporaryFolder.getRoot() } ] as WorkingDirectoryProvider
+
+    }
+
+    def 'gets the working directory'() {
+        when:
+        EnvironmentData data = new WorkingDirectoryEnvironmentCollector(workingDirectoryProvider).collectEnvironmentData()
+
+        then:
+        data.collectorName == WorkingDirectoryEnvironmentCollector.class.getSimpleName()
+        data.environmentResults.keySet() == [ WorkingDirectoryEnvironmentCollector.WORKING_DIRECTORY ] as Set
+        data.environmentResults[WorkingDirectoryEnvironmentCollector.WORKING_DIRECTORY] == temporaryFolder.root.absolutePath
+
+    }
+}
