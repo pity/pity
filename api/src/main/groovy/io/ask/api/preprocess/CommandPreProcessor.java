97266a2f0dac219329df36f7de1b37fe630d2f92
Ethan Hall <ethan@ehdev.io>
2015-05-14 20:54:38 -0700

    Removing non-useful annotation

--- api/src/main/groovy/io/ask/api/preprocess/CommandPreProcessor.java
+++ api/src/main/groovy/io/ask/api/preprocess/CommandPreProcessor.java
@@ -1,11 +1,8 @@
 package io.ask.api.preprocess;
 
-import javax.annotation.Nonnull;
-
 public interface CommandPreProcessor {
 
     int commandPrecedence();
 
-    @Nonnull
     CommandOptions processCommand(CommandOptions command);
 }
