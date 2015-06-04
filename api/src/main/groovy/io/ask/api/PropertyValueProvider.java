8393dbfbab765a44866f65beef2cab5a81ff60c4
Ethan Hall <ethan@ehdev.io>
2015-06-03 19:59:12 -0700

    Adding javadocs

--- api/src/main/groovy/io/ask/api/PropertyValueProvider.java
+++ api/src/main/groovy/io/ask/api/PropertyValueProvider.java
@@ -1,5 +1,16 @@
 package io.ask.api;
 
+/**
+ * {@link PropertyValueProvider} will load in any property files located in /META-INF/ask-plugins and provide the
+ * properties accessible by {@link PropertyValueProvider#getProperty(String)}. If multiple property files have the same
+ * key then there is no guarantee to which will be used.
+ */
 public interface PropertyValueProvider {
-    public abstract String getProperty(String name);
+
+    /**
+     * Get a property from the loaded property files
+     * @param name property name
+     * @return property value.
+     */
+    String getProperty(String name);
 }
