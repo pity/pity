789420f6f2e5b8177dde625f3fe08c145f49342e
Ethan Hall <ethan@ehdev.io>
2015-06-19 10:49:16 -0700

    Changing default log level, and can now publish

--- bootstrap/src/main/resources/logback.groovy
+++ bootstrap/src/main/resources/logback.groovy
@@ -1,11 +1,12 @@
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.classic.Level
 
 appender("STDOUT", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
     }
 }
-logger('org.reflections.vfs', ERROR)
-logger('org.reflections', ERROR)
-root(DEBUG, ["STDOUT"])
+logger('org.reflections.vfs', Level.ERROR)
+logger('org.reflections', Level.ERROR)
+root(Level.INFO, ["STDOUT"])
