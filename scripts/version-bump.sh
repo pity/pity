8d84d2f0ad25c421db8bac54a6519f44c06ce1bd
Ethan Hall <ethan@ehdev.io>
2015-06-20 15:53:11 -0700

    Adding params

--- scripts/version-bump.sh
+++ scripts/version-bump.sh
@@ -7,7 +7,7 @@ echo "Tagging version $VERSION"
 git tag "V$VERSION"
 git push --tags
 
-$DIR/../gradlew -Pbintray.publish=true bintrayUpload
+$DIR/../gradlew -Pbintray.publish=true -Pbintray_user=$BINTRAY_USER -Pbintray_key=$BINTRAY_PASSWORD bintrayUpload
 
 VERSION=`echo $VERSION | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g'`
 echo "Next version is $VERSION"
