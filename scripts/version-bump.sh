6f5877f89e61743c3ada32bc5dc96b96df5da62a
Ethan Hall <ethan@ehdev.io>
2015-06-20 15:43:33 -0700

    Preping for automatic release

--- scripts/version-bump.sh
+++ scripts/version-bump.sh
@@ -4,14 +4,14 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 VERSION=$(awk '{ printf "%s", $0 }' $DIR/../VERSION)
 echo "Tagging version $VERSION"
-#git tag "V$VERSION"
-#git push --tags
+git tag "V$VERSION"
+git push --tags
 
 $DIR/../gradlew -Pbintray.publish=true bintrayUpload
 
 VERSION=`echo $VERSION | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g'`
 echo "Next version is $VERSION"
-#git commit -a -m "Bumping version to $VERSION \n[ci skip]"
-#git push
+git commit -a -m "Bumping version to $VERSION \n[ci skip]"
+git push
 
 echo $VERSION > $DIR/../VERSION
