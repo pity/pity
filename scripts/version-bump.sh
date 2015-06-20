678b0192289cdc55fcf545afa9f9d6098c99b883
Ethan Hall <ethan@ehdev.io>
2015-06-20 15:56:47 -0700

    fixing push order

--- scripts/version-bump.sh
+++ scripts/version-bump.sh
@@ -5,13 +5,14 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 VERSION=$(awk '{ printf "%s", $0 }' $DIR/../VERSION)
 echo "Tagging version $VERSION"
 git tag "V$VERSION"
-git push --tags
 
 $DIR/../gradlew -Pbintray.publish=true -Pbintray_user=$BINTRAY_USER -Pbintray_key=$BINTRAY_PASSWORD bintrayUpload
 
 VERSION=`echo $VERSION | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g'`
 echo "Next version is $VERSION"
+echo $VERSION > $DIR/../VERSION
+
 git commit -a -m "Bumping version to $VERSION \n[ci skip]"
 git push
+git push --tags
 
-echo $VERSION > $DIR/../VERSION
