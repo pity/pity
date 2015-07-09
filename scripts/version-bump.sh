daecb3bfc10a383bac762fda4aac0bf551914b8c
Ethan Hall <ethan@ehdev.io>
2015-07-08 22:13:54 -0700

    Preventing secure logging in ci

--- scripts/version-bump.sh
+++ scripts/version-bump.sh
@@ -6,7 +6,7 @@ VERSION=$(awk '{ printf "%s", $0 }' $DIR/../VERSION)
 echo "Tagging version $VERSION"
 git tag "V$VERSION"
 
-$DIR/../gradlew -Pbintray.publish=true -Pbintray_user=$BINTRAY_USER -Pbintray_key=$BINTRAY_PASSWORD bintrayUpload
+$DIR/../gradlew -Pbintray.publish=true bintrayUpload
 
 VERSION=`echo $VERSION | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g'`
 echo "Next version is $VERSION"
